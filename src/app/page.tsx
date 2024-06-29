f9a9422964162d0a86b4681937df34847650653c
이예슬 <20152917@sungshin.ac.kr>
2024-06-29 16:49:48 +0900

    chore: DefaultPage 컴포넌트 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import {
   Container,
   ScrollArea,
@@ -8,37 +6,13 @@ import {
   TypographyStylesProvider,
 } from '@mantine/core'
 import { ImageListing } from '~/ui/detail/ImageListing'
+import { DefaultPage } from '~/ui/detail/DefaultPage'
 
-// TODO: api 연결
-const dummyImages = [
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-  undefined,
-]
 type Props = {}
 export default function Home(props: Props) {
   return (
     <Container>
-      <div style={{ paddingBottom: '30px', paddingTop: '25px' }}>
-        <Title order={2} style={{ paddingBottom: '5px' }}>
-          어떤 느낌의 여행지를
-          <br />
-          좋아하시나요?
-        </Title>
-        <Text style={{ fontSize: '13px', whiteSpace: 'nowrap' }}>
-          선택한 3개 이미지 또는 직접 등록한 이미지로 맞춤 추천됩니다.
-        </Text>
-      </div>
-      <div>
-        <ScrollArea>
-          <ImageListing images={dummyImages} showAddButton={true} />
-        </ScrollArea>
-      </div>
+      <DefaultPage />
     </Container>
   )
 }
