1bbf11c03e6a1934b22b91bcbd2f3fb005246193
ocean <ocean96@interparktriple.com>
2024-07-02 20:15:18 +0900

    feat: add product type

--- src/utils/pickle.ts
+++ src/utils/pickle.ts
@@ -1,4 +1,5 @@
 import { ResourceType } from './resource-types'
+import { Geotag, Image } from '~/utils/triple-types'
 
 export interface PickleImage {
   id: string
@@ -7,4 +8,13 @@ export interface PickleImage {
   info: string
   region: { name: string }
   resourceType: ResourceType
+}
+
+/** (AI 돌리기 전) DB에 들어갈 POI 및 TNA 원천 데이터 */
+export interface Product {
+  id: string
+  type: ResourceType
+  name: string
+  region: Geotag & { name?: string }
+  image: Image
 }
